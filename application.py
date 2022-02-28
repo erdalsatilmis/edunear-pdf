88feb5908d806f85fe67cddd315cf1f60d655be8
erdalsatilmis <6634403@gmail.com>
2022-02-28 12:02:38 +0300

    Update application.py

--- application.py
+++ application.py
@@ -40,8 +40,8 @@ def convertPDF():
 
 
 # start flask app
-if __name__ == "__main__":
- app.run(host="0.0.0.0", port=8080)
+#if __name__ == "__main__":
+# app.run(host="0.0.0.0", port=8080)
 #if __name__ == "__main__":
 #    app.run(ssl_context='adhoc')
 
